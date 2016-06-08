c4abc07d4bbdf4d12919105a007b32d5bf9d8f7e
Manuel <manuel@wiris.com>
2016-06-08 08:15:56 +0200

    3.58.0.1306  Stable version

--- version.php
+++ version.php
@@ -16,9 +16,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016051900;
-$plugin->release = '3.57.3.1305';
+$plugin->version   = 2016060800;
+$plugin->release = '3.58.0.1306';
 $plugin->requires  = 2014050800;
 $plugin->component = 'atto_wiris';
-$plugin->dependencies = array ('filter_wiris' => 2016051900);
+$plugin->dependencies = array ('filter_wiris' => 2016060800);
 $plugin->maturity = MATURITY_STABLE;
