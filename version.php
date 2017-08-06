1a4e6058fb2f740df0e5b8e2d766caeb00f7a273
Manuel <manuel@wiris.com>
2017-08-07 00:04:51 +0200

    2017080700 development version

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017080500;
+$plugin->version = 2017080700;
 
 $plugin->requires = 2014050800;
 $plugin->component = 'atto_wiris';
-$plugin->dependencies = array ('filter_wiris' => 2017080500);
+$plugin->dependencies = array ('filter_wiris' => 2017080700);
 $plugin->maturity = MATURITY_BETA;
