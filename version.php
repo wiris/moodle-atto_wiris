9cfddabe97d9a630bc0f85949b0b95ce0669792a
Manuel <manuel@wiris.com>
2018-01-02 12:28:34 +0100

    2018010100 Stable version

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2018010100;
-
+$plugin->release = '4.10.0.1382';
 $plugin->requires = 2014050800;
 $plugin->component = 'atto_wiris';
 $plugin->dependencies = array ('filter_wiris' => 2018010100);
-$plugin->maturity = MATURITY_BETA;
+$plugin->maturity = MATURITY_STABLE;
