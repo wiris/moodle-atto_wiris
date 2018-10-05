b5d75f4fda89ce880502e1f98a84bc97c07aff40
Jenkins <jenkins@wiris.com>
2018-10-05 04:04:30 +0200

    2018100500 development version

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018100400;
+$plugin->version = 2018100500;
 
 $plugin->requires = 2014050800;
 $plugin->component = 'atto_wiris';
-$plugin->dependencies = array ('filter_wiris' => 2018100400);
+$plugin->dependencies = array ('filter_wiris' => 2018100500);
 $plugin->maturity = MATURITY_BETA;
