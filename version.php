9465e58160fb9003e321954d5e7cff8d06c3eb21
Manuel Cagigas <manuel@wiris.com>
2019-05-13 11:03:06 +0200

    2019051300 stable version

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019090500;
-$plugin->release = '7.12.0.1413';
+$plugin->version = 2019051300;
+$plugin->release = '7.13.0';
 $plugin->requires = 2014050800;
 $plugin->component = 'atto_wiris';
-$plugin->dependencies = array ('filter_wiris' => 2019040800);
+$plugin->dependencies = array ('filter_wiris' => 2019051300);
 $plugin->maturity = MATURITY_BETA;
