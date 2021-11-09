2e93847ac30bfca6f261a4a19341f8acbb5b4136
Dani Canet <dcanet@wiris.com>
2021-11-09 15:30:05 +0100

    Release 7.27.1 (9th nov. 2021)

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021072200;
-$plugin->release = '7.27.0';
+$plugin->version = 2021110900;
+$plugin->release = '7.27.1';
 $plugin->requires = 2014050800;
 $plugin->component = 'atto_wiris';
-$plugin->dependencies = array ('filter_wiris' => 2021061800);
+$plugin->dependencies = array ('filter_wiris' => 2021110900);
 $plugin->maturity = MATURITY_STABLE;
