a8747c6cd7e6a97b79341f3abd3c622f6a2ba6c0
icaparros-at-wiris <isaac.caparros@wiris.com>
2022-06-20 11:33:47 +0200

    build(version) publish 7.29.0

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022040800;
-$plugin->release = '7.28.0';
+$plugin->version = 2022062000;
+$plugin->release = '7.29.0';
 $plugin->requires = 2014050800;
 $plugin->component = 'atto_wiris';
-$plugin->dependencies = array ('filter_wiris' => 2022040800);
+$plugin->dependencies = array ('filter_wiris' => 2022062000);
 $plugin->maturity = MATURITY_STABLE;
