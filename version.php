f96029b028cfc6c555c8ad8fd660bf638c6720d2
carla-at-wiris <carla@wiris.com>
2023-06-12 16:30:06 +0200

    build(version): publish #v.8.3.0

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023042400;
-$plugin->release = '8.2.6';
+$plugin->version = 2023061200;
+$plugin->release = '8.3.0';
 $plugin->requires = 2014050800;
 $plugin->component = 'atto_wiris';
-$plugin->dependencies = array ('filter_wiris' => 2023042400);
+$plugin->dependencies = array ('filter_wiris' => 2023061200);
 $plugin->maturity = MATURITY_STABLE;
