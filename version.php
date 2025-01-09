b9f6937e5761b71606e1406b9f57ae782e5c21e5
xjiang-at-wiris <125269853+xjiang-at-wiris@users.noreply.github.com>
2025-01-09 10:46:10 +0100

    build: publish #8.8.2

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024110400;
-$plugin->release = '8.8.1';
+$plugin->version = 2025010900;
+$plugin->release = '8.8.2';
 $plugin->requires = 2014050800;
 $plugin->component = 'atto_wiris';
-$plugin->dependencies = ['filter_wiris' => 2024110400];
+$plugin->dependencies = ['filter_wiris' => 2025010900];
 $plugin->maturity = MATURITY_STABLE;
